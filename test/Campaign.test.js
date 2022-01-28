b25bef8e7d242679c777cdc994d009e4b1af379f
Ivan Pavic <ivan.pavic@usi.ch>
2022-01-28 11:53:32 +0100

    implemented some tests

---
+++ test/Campaign.test.js
@@ -0,0 +1,40 @@
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+const web3 = new Web3(ganache.provider());
+
+const compiledFactory = require("../ethereum/build/CampaignFactory.json");
+const compiledCampaign = require("../ethereum/build/Campaign.json");
+
+let accounts;
+let factory;
+let campaign;
+let campaignAddress;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+
+  factory = await new web3.eth.Contract(compiledFactory.abi)
+    .deploy({ data: compiledFactory.evm.bytecode.object })
+    .send({ from: accounts[0], gas: "1500000" });
+
+  await factory.methods.createCampaign("100").send({
+    from: accounts[0],
+    gas: "1500000",
+  }); // 100 Wei
+
+  [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
+  campaign = await new web3.eth.Contract(compiledCampaign.abi, campaignAddress);
+});
+
+describe("Campaigns", () => {
+  it("deploy a factory and a campaign", () => {
+    assert.ok(factory.options.address);
+    assert.ok(campaign.options.address);
+  });
+
+  it("marks caller as the campaign manager", async () => {
+    const manager = await campaign.methods.manager().call();
+    assert.equal(accounts[0], manager);
+  });
+});
